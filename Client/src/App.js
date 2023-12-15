2a74a1e685f6f4c19e94b4730e6805f9317f6fac
bassem <Bassembelhajbouubaker11@gmail.com>
2023-12-15 07:31:14 +0100

    add posts

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+      <a href="" classeName="logo"> MyBlog </a>
+      <nav>
+      <a href="" > Login </a>
+      <a href="" > Register </a>
+      </nav>
       </header>
-    </div>
+      <div classeName="post">
+      <div classeName="image">       
+      <img src="https://techcrunch.com/wp-content/uploads/2023/06/OpenAI-logo-symmetry.jpg?w=850&h=492&crop=1&fbclid=IwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Qepard_LaunchPadCheck.webp" alt=""/>
+         </div>
+      <div classeName="texts">
+      <h2>OpenAI inks deal with Axel Springer on licensing news for model training</h2>
+          <p>Many, if not most, generative AI tech vendors argue that fair use entitles them to train AI models on copyrighted material scraped from the internet — even if they don’t get permission from the rightsholders. But some vendors, such as OpenAI, are hedging their bets — perhaps wary of the outcome of pending relevant lawsuits.
+             OpenAI today announced that it’s reached an agreement with Axel Springer, the Berlin-based owner of publications including Business Insider and Politico, to train its generative AI models on the publisher’s content and add recent Axel Springer-published articles to OpenAI’s viral AI-powered chatbot ChatGPT.
+        </p>
+       </div>
+      </div>
+      <div classeName="post">
+        <div classeName="image">       
+      <img src="https://techcrunch.com/wp-content/uploads/2023/06/OpenAI-logo-symmetry.jpg?w=850&h=492&crop=1&fbclid=IwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Qepard_LaunchPadCheck.webp" alt=""/>
+         </div>
+      <div classeName="texts">
+      <h2>OpenAI inks deal with Axel Springer on licensing news for model training</h2>
+          <p>Many, if not most, generative AI tech vendors argue that fair use entitles them to train AI models on copyrighted material scraped from the internet — even if they don’t get permission from the rightsholders. But some vendors, such as OpenAI, are hedging their bets — perhaps wary of the outcome of pending relevant lawsuits.
+             OpenAI today announced that it’s reached an agreement with Axel Springer, the Berlin-based owner of publications including Business Insider and Politico, to train its generative AI models on the publisher’s content and add recent Axel Springer-published articles to OpenAI’s viral AI-powered chatbot ChatGPT.
+        </p>
+       </div>
+      </div>
+      <div classeName="post">
+      <div classeName="image">       
+      <img src="https://techcrunch.com/wp-content/uploads/2023/06/OpenAI-logo-symmetry.jpg?w=850&h=492&crop=1&fbclid=IwAR2GOgpgav4DTRwWOVxpCXc-VzrTMxkro-HcWwpQd02msGfh153WdGHaQ5Qepard_LaunchPadCheck.webp" alt=""/>
+         </div>
+      
+      <div classeName="texts">
+      <h2>OpenAI inks deal with Axel Springer on licensing news for model training</h2>
+          <p>Many, if not most, generative AI tech vendors argue that fair use entitles them to train AI models on copyrighted material scraped from the internet — even if they don’t get permission from the rightsholders. But some vendors, such as OpenAI, are hedging their bets — perhaps wary of the outcome of pending relevant lawsuits.
+             OpenAI today announced that it’s reached an agreement with Axel Springer, the Berlin-based owner of publications including Business Insider and Politico, to train its generative AI models on the publisher’s content and add recent Axel Springer-published articles to OpenAI’s viral AI-powered chatbot ChatGPT.
+        </p>
+       </div>
+      </div>
+      
+     
+    </main>
   );
 }
 
