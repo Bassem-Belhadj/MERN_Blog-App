91dd1f26f6e0ccf27f11683d126fb4977346d7b0
bassem <bassembelhajboubaker11@gmail.com>
2024-01-02 19:38:49 +0100

    update posts

--- Client/src/Pages/CreatePost.js
+++ Client/src/Pages/CreatePost.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 function CreatePost(){
+    const [title , setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    function createNewPost (ev){
+        ev.preventDefault();
+    }
     return(
-       <form>
-        <input type="title" placeholder="{'Title'}"/>
-        <input type="summary " placeholder="{'Summary'}"/>
+       <form onSubmit={createNewPost}>
+        <input type="title" 
+        placeholder="{'Title'}" 
+        value={title}
+        onChange={ev => setTitle(ev.target.value)}
+        />
+        <input type="summary "
+         placeholder="{'Summary'}" 
+         value={summary} 
+         onChange={ev=> setSummary(ev.target.value)}
+         />
         <input type="file"/>
-        <ReactQuill/>
+        <ReactQuill value={content} 
+        onChange={newValue => setContent(newValue)}
+        />
+        <button style={{margin:'5px'}}>create post</button>
        </form>
     )
 }
