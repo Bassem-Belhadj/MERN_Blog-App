27029a22b1200564c5e1792fff3fca47e64dbc3b
bassem <bassembelhajboubaker11@gmail.com>
2024-01-06 21:46:47 +0100

    Create post

--- Client/src/Pages/CreatePost.js
+++ Client/src/Pages/CreatePost.js
@@ -1,45 +1,49 @@
-import { useState } from 'react';
-import ReactQuill from 'react-quill';
+import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
+import {useState} from "react";
+import {Navigate} from "react-router-dom";
+import Editor from "../Editor";
 
-function CreatePost(){
-    const [title , setTitle] = useState('');
-    const [summary, setSummary] = useState('');
-    const [content, setContent] = useState('');
-    const [file, setFile]= useState('');
-   async function createNewPost (ev){
-        const data = new FormData(); 
-        data.set('title', title);
-        data.set('summary', summary);
-        data.set('content', content);
-        data.set('file', files[0]);
-        ev.preventDefault();
-     const response = await fetch('http://localhost:4000/login', {
-      method:'POST',
-      body:data,
-       
-     })
+export default function CreatePost() {
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  async function createNewPost(ev) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'POST',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
     }
-    return(
-       <form onSubmit={createNewPost}>
-        <input type="title" 
-        placeholder="{'Title'}" 
-        value={title}
-        onChange={ev => setTitle(ev.target.value)}
-        />
-        <input type="summary "
-         placeholder="{'Summary'}" 
-         value={summary} 
-         onChange={ev=> setSummary(ev.target.value)}
-         />
-        <input type="file" 
-        value={file} 
-        onChange={ev=>setFile(ev.target.files)}/>
-        <ReactQuill value={content} 
-        onChange={newValue => setContent(newValue)}
-        />
-        <button style={{margin:'5px'}}>create post</button>
-       </form>
-    )
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+  return (
+    <form onSubmit={createNewPost}>
+      <input type="title"
+             placeholder={'Title'}
+             value={title}
+             onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary"
+             placeholder={'Summary'}
+             value={summary}
+             onChange={ev => setSummary(ev.target.value)} />
+      <input type="file"
+             onChange={ev => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{marginTop:'5px'}}>Create post</button>
+    </form>
+  );
 }
-export default CreatePost
