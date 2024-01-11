2c301f87019e6a85aef3037335d66542d15ba7c7
bassem <bassembelhajboubaker11@gmail.com>
2024-01-11 23:11:02 +0100

    update posts

--- Client/src/Pages/IndexPage.js
+++ Client/src/Pages/IndexPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Post from "../Post";
 function IndexPage(){
+    useEffect(()=>{
+       fetch('/post').then(response=>{
+        response.json().then(posts =>{
+            console.log(posts);
+        });
+       });
+    },[]);
     return(
         <>
         <Post/>
